a202bddd0927dd95660d579f374f9cff12bf852b
Blanka 3719 <mmohammed>
2026-05-26 14:56:42 +0200

    Acceuil et jeu et main

--- main.java
+++ main.java
@@ -2,10 +2,8 @@ public class main {
 
     public static void main(String[] args) {
 
-        // Création du menu d'accueil
         accueil accueil = new accueil();
 
-        // Lancement du menu principal
         accueil.afficherMenu();
     }
 }
